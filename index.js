49650655ac5e5ae5ab7752e7b1999b52b5398dbb
Saffeya Hinkson <sthinkson@hotmail.com>
2018-01-05 19:30:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+function logShout(string) {
+  console log (string.uppercase.toUpperCase)
+}
+function logWhisper(string) {
+  console log (string.lowercase.toLowerCase)
+}
+function sayHiToGrandma(string) {
+  mixedCase.toLowerCase()
+}
+function sayHiToGrandma(string) {
+  mixedCase.toUpperCase()
+}
